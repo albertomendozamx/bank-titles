d0cae059eadc282f7123e3f19de187003f0397f2
Neris <mendozaene@gmail.com>
2023-01-20 11:31:21 -0600

    validations

--- src/routes/titulos.ts
+++ src/routes/titulos.ts
@@ -23,12 +23,20 @@ router.get('/:id', (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const newUserTitle = inspectTitle(req.body)
-    const savedUserTitle = await titleService.addTitle(newUserTitle)
-    return res.status(201).send({
-      status: true,
-      message: 'Created',
-      data: { id: savedUserTitle, ...newUserTitle }
-    })
+    const hasTitle = await titleService.findByTitle(newUserTitle.idtitulo)
+    if (!hasTitle) {
+      const savedUserTitle = await titleService.addTitle(newUserTitle)
+      return res.status(201).send({
+        status: true,
+        message: 'Created',
+        data: { id: savedUserTitle, ...newUserTitle }
+      })
+    } else {
+      return res.status(400).send({
+        status: false,
+        message: 'User already has this title'
+      })
+    }
   } catch (Error) {
     return res.status(400).send({ status: false, message: 'Error', error: `${Error}` })
   }
@@ -40,15 +48,23 @@ router.delete('/:id', async (req, res) => {
   if (!titulo)
     return res.status(404).send({ status: false, message: 'Not found', data: [] })
 
-  const wasDeleted = await titleService.deleteByID(id)
-  if (!wasDeleted)
-    return res.status(400).send({ status: false, message: 'Delete failed', data: [] })
+  try {
+    const wasDeleted = await titleService.deleteByID(id)
+    if (!wasDeleted)
+      return res.status(400).send({ status: false, message: 'Delete failed', data: [] })
 
-  return res.status(200).send({
-    status: true,
-    message: `${id} was deleted successfully`,
-    data: []
-  })
+    return res.status(200).send({
+      status: true,
+      message: `${id} was deleted successfully`,
+      data: []
+    })
+  } catch (Error) {
+    return res.status(400).send({
+      status: false,
+      message: `${id} was not deleted`,
+      data: []
+    })
+  }
 })
 
 router.patch('/:id', async (req, res) => {
